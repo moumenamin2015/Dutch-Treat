23e9c1d55304272116f8b27758a2a7232f53f7f8
Moumen Amin <Moumen.Amin@hotmail.com>
2019-05-23 14:24:52 +0200

    adding images

---
+++ CorePlayground/Data/Repositories/DutchRepository.cs
@@ -0,0 +1,35 @@
+﻿using CorePlayground.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CorePlayground.Data.Repositories
+{
+    public class DutchRepository : IDutchRepository
+    {
+        private readonly DutchContext context;
+
+        public DutchRepository(DutchContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return context.Products
+                .OrderBy(p => p.Title).ToList();
+        }
+
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+            return context.Products
+                .Where(p => p.Category == category).ToList();
+        }
+
+        public bool SaveAll()
+        {
+            return context.SaveChanges() > 0;
+        }
+    }
+}
---
+++ CorePlayground/Data/Repositories/IDutchRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using CorePlayground.Data.Entities;
+
+namespace CorePlayground.Data.Repositories
+{
+    public interface IDutchRepository
+    {
+        IEnumerable<Product> GetAllProducts();
+        IEnumerable<Product> GetProductsByCategory(string category);
+        bool SaveAll();
+    }
+}
